58ee6d3f6fa2406c000b9b7b205c72dedab7d1d7
drobin03 <drobin03@uoguelph.ca>
2013-10-10 14:27:46 -0400

    Added phone gap application. Currently has login page.

---
+++ ResearchPal/platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/dan/Desktop/Fall_2013/ResearchPal/ResearchPal/platforms/android/bin/AndroidManifest.xml : \
+/home/dan/Desktop/Fall_2013/ResearchPal/ResearchPal/platforms/android/AndroidManifest.xml \
---
+++ ResearchPal/platforms/android/bin/classes.dex.d
@@ -0,0 +1,10 @@
+/home/dan/Desktop/Fall_2013/ResearchPal/ResearchPal/platforms/android/bin/classes.dex : \
+/home/dan/Desktop/Fall_2013/ResearchPal/ResearchPal/platforms/android/bin/classes/com/phonegap/hello_world/HelloWorld.class \
+/home/dan/Desktop/Fall_2013/ResearchPal/ResearchPal/platforms/android/bin/classes/com/phonegap/hello_world/R$drawable.class \
+/home/dan/Desktop/Fall_2013/ResearchPal/ResearchPal/platforms/android/bin/classes/com/phonegap/hello_world/R$string.class \
+/home/dan/Desktop/Fall_2013/ResearchPal/ResearchPal/platforms/android/bin/classes/com/phonegap/hello_world/R.class \
+/home/dan/Desktop/Fall_2013/ResearchPal/ResearchPal/platforms/android/bin/classes/com/phonegap/hello_world/R$xml.class \
+/home/dan/Desktop/Fall_2013/ResearchPal/ResearchPal/platforms/android/bin/classes/com/phonegap/hello_world/R$attr.class \
+/home/dan/Desktop/Fall_2013/ResearchPal/ResearchPal/platforms/android/bin/classes/com/phonegap/hello_world/R$layout.class \
+/home/dan/Desktop/Fall_2013/ResearchPal/ResearchPal/platforms/android/bin/classes/com/phonegap/hello_world/BuildConfig.class \
+/home/dan/Desktop/Fall_2013/ResearchPal/ResearchPal/platforms/android/bin/dexedLibs/cordova-3.0.0-eb839150b63621a36d540532a09ba56e.jar \
