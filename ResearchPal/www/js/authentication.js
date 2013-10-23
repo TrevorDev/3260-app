5438a03d80c4c91ddaf86911c2a9518774dd9c70
drobin03 <drobin03@uoguelph.ca>
2013-10-23 14:16:27 -0400

    getting ajax to work on phone..  not working yet

--- ResearchPal/www/js/authentication.js
+++ ResearchPal/www/js/authentication.js
@@ -3,14 +3,11 @@ function validateUser(pinEntered){
 		type: "POST",
 		url: 'http://127.0.0.1/appAuth',
 		crossDomain: true,
-		dataType: 'json',
 		data: {
 			pin: pinEntered
 		},
 		success: function(data) {
-			var jsonData = $.parseJSON(data);
-			var valid = jsonData.valid;
-			alert("yay" + valid);
+			console.log("validating user.... returned " + data);
 	    	},
 	    	error: onError
 	 });
@@ -24,3 +21,4 @@ alert("errorThrown: " + errorThrown);
 // can't remember if this works:
 alert(jqXHR.statusText);
 }
+
--- server/routes/appAuth/appAuth.js
+++ server/routes/appAuth/appAuth.js
@@ -6,5 +6,14 @@ exports.validateUser = function(req, res, next) {
 
     /* view = 'home';
     exports.render(req, res, next, 'researcherPortal/' + view); */
-    res.send({"valid": "yes"});
-};
+    res.header('Access-Control-Allow-Origin', "*");     // TODO - Make this more secure!!
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST');
+    res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept');
+	
+    if(!req.session.test){
+	req.session.test='hit';
+    }else{
+	req.session.test='hit2';
+    }
+    res.send(req.session.test);
+}
