28d22b8aa18b515ca4004d01082289bbdbb4144f
root <root@dan-Lenovo-G550.(none)>
2013-10-23 12:50:36 -0400

    issue with cross domain post requests

--- ResearchPal/platforms/android/bin/AndroidManifest.xml.d
+++ ResearchPal/platforms/android/bin/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/home/dan/Desktop/Fall_2013/ResearchPal/ResearchPal/platforms/android/bin/AndroidManifest.xml : \
-/home/dan/Desktop/Fall_2013/ResearchPal/ResearchPal/platforms/android/AndroidManifest.xml \
+/home/dan/Desktop/Fall_2013/ResearchPal/3260-app/ResearchPal/platforms/android/bin/AndroidManifest.xml : \
+/home/dan/Desktop/Fall_2013/ResearchPal/3260-app/ResearchPal/platforms/android/AndroidManifest.xml \
