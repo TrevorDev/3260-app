4d24d38f3ade822a56e9e2531273ea350d06220a
Trevor Baron <trevorjbaron@gmail.com>
2013-10-31 21:53:05 -0400

    add get groups

--- server/custom_modules/database/userModel.js
+++ server/custom_modules/database/userModel.js
@@ -3,6 +3,18 @@ var rek = require('rekuire');
 var db = rek('database.js');
 
 
+exports.getResearchersGroups= function(username) {
+	var conn = db.getConnection();
+	conn.query('SELECT * from group,researcher where ownerID = userID and username = '+conn.escape(username), function(err, rows, fields) {
+	  if (err) throw err;
+	  if(rows.length>0){
+	  	callback(true);
+	  }else{
+	  	callback(false);
+	  }
+	});
+}
+
 exports.authResearcher= function(username, password, callback) {
 	var conn = db.getConnection();
 	conn.query('SELECT * from researcher where username = '+conn.escape(username)+' and password='+conn.escape(password), function(err, rows, fields) {
---
+++ server/routes/user/user.js
@@ -0,0 +1,5 @@
+var fs = require('fs');
+var ejs = require('ejs');
+var rek = require('rekuire');
+
+var userM = rek('userModel.js');
