c8d4736e1ba8ca42b806d90bb1d0ca92adab3515
drobin03 <drobin03@uoguelph.ca>
2013-11-06 12:56:00 -0500

    fixed another sql error

--- server/custom_modules/database/userModel.js
+++ server/custom_modules/database/userModel.js
@@ -54,8 +54,8 @@ exports.getResearcher= function(userID,callback) {
   var conn = db.getConnection();
   conn.query("Select researcher.userID as researcherID " +
                 "from pal.group, participant, researcher " +
-                "where participant.groupID = group.groupID" +
-                  "and group.ownerID = researcher.userID" +
+                "where participant.groupID = group.groupID " +
+                  "and group.ownerID = researcher.userID " +
                   "and participant.userID = " + conn.escape(userID), function(err, rows, fields) {
     if (err) throw err;
     if(rows.length>0){
