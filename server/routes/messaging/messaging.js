802bcd8538b53bfc34576e031ee6e3c43b2feed3
drobin03 <drobin03@uoguelph.ca>
2013-11-06 12:23:40 -0500

    not yet

--- server/routes/messaging/messaging.js
+++ server/routes/messaging/messaging.js
@@ -20,7 +20,7 @@ exports.addRecording = function(req, res, next) {
             if (success){
                 msgFrom = userid;
             }
-            res.send(msgFrom);
+            res.send("User id " + msgFrom);
         });
         /*var msgTo = userM.getResearcher();
         messageM.storeMessage(msgFrom, msgTo, data);
