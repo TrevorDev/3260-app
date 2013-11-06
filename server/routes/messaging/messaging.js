8d6e9ce28042c3e50fcab9e7a89da2152ebba8ec
drobin03 <drobin03@uoguelph.ca>
2013-11-06 13:17:57 -0500

    storing worked! now trying to take out res headers.

--- server/routes/messaging/messaging.js
+++ server/routes/messaging/messaging.js
@@ -7,12 +7,12 @@ var userM = rek('userModel.js');
 exports.addRecording = function(req, res, next) {
 
     /* view = 'home';
-    exports.render(req, res, next, 'researcherPortal/' + view); */
+    exports.render(req, res, next, 'researcherPortal/' + view); 
     res.header('Access-Control-Allow-Origin', "*");     // TODO - Make this more secure!!
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST');
     res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept');
+    */
     var pin;
-    /* res.send(req.files); */
 
     fs.readFile(req.files.file.path, function (err, data) {
         var msgFrom = req.session.userID;
@@ -21,27 +21,13 @@ exports.addRecording = function(req, res, next) {
             var msgTo = "false";
             if (success){
                 msgTo = row.researcherID;
+                messageM.store(msgFrom, msgTo, data, function(success){
+                    if (success){
+                        res.send('success');
+                    }
+                    res.send('failed');
+                });
             }
-            console.log("DATA  " + data);
-            console.log("User id " + msgFrom + " Researcher ID " + msgTo);
-
-            messageM.store(msgFrom, msgTo, data, function(success){
-                if (success){
-                    res.send('success');
-                }
-                res.send('failed');
-            });
         });
-        /*var msgTo = userM.getResearcher();
-        messageM.storeMessage(msgFrom, msgTo, data);
-        */
     });
-    /*messageM.store(req.body.pin,function(success){
-        if(success){
-            req.session.auth=req.body.pin;
-            res.send("success");
-        }else{
-            res.send("false");
-        }
-    });*/
 }
