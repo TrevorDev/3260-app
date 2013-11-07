7cc95c7bc6ed5db6e4d61af92901fb60f94a1868
drobin03 <drobin03@uoguelph.ca>
2013-11-07 12:56:06 -0500

    small change

--- server/routes/messaging/messaging.js
+++ server/routes/messaging/messaging.js
@@ -90,8 +90,8 @@ exports.getRecording = function(req,res,next) {
         var filePath = path.join(process.cwd(), '/uploads/', req.params.fileName);
         var stat = fs.statSync(filePath);
 
+
         console.log(stat.size);
-        
         res.writeHead(200, {
             'Content-Type': 'audio/wav',
             'Content-Length': stat.size
