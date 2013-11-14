28e9912eb057da8880f21e6a1df60543719103da
drobin03 <drobin03@uoguelph.ca>
2013-11-14 16:34:37 -0500

    phongap files ignore

--- ResearchPal/platforms/android/bin/HelloWorld-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/dan/Desktop/Fall_2013/ResearchPal/3260-app/ResearchPal/platforms/android/bin/HelloWorld-debug-unaligned.apk : \
-/home/dan/Desktop/Fall_2013/ResearchPal/3260-app/ResearchPal/platforms/android/bin/HelloWorld.ap_ \
-/home/dan/Desktop/Fall_2013/ResearchPal/3260-app/ResearchPal/platforms/android/bin/classes.dex \
-/home/dan/Desktop/Fall_2013/ResearchPal/3260-app/ResearchPal/platforms/android/libs/cordova-3.0.0.jar \
