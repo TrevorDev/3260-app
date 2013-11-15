38290a1c5474ae0e0294461771d6b2dd02d84184
ahnh <ahn.heesung@gmail.com>
2013-11-15 12:59:14 -0500

    fuckthisgit

--- ResearchPal/platforms/android/bin/HelloWorld-debug-unaligned.apk.d
+++ ResearchPal/platforms/android/bin/HelloWorld-debug-unaligned.apk.d
@@ -1,4 +1,4 @@
-/home/dan/Desktop/Fall_2013/ResearchPal/3260-app/ResearchPal/platforms/android/bin/HelloWorld-debug-unaligned.apk : \
-/home/dan/Desktop/Fall_2013/ResearchPal/3260-app/ResearchPal/platforms/android/bin/HelloWorld.ap_ \
-/home/dan/Desktop/Fall_2013/ResearchPal/3260-app/ResearchPal/platforms/android/bin/classes.dex \
-/home/dan/Desktop/Fall_2013/ResearchPal/3260-app/ResearchPal/platforms/android/libs/cordova-3.0.0.jar \
+/home/ahnh/3260-app/ResearchPal/platforms/android/bin/HelloWorld-debug-unaligned.apk : \
+/home/ahnh/3260-app/ResearchPal/platforms/android/bin/HelloWorld.ap_ \
+/home/ahnh/3260-app/ResearchPal/platforms/android/bin/classes.dex \
+/home/ahnh/3260-app/ResearchPal/platforms/android/libs/cordova-3.0.0.jar \
