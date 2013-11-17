7a3ee465f7277e51f33b21fa59cd5bde36ecd5d8
drobin03 <drobin03@uoguelph.ca>
2013-11-17 16:41:08 -0500

    added rules to stop tracking some system phonegap files

--- ResearchPal/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/ahnh/3260-app/ResearchPal/platforms/android/bin/AndroidManifest.xml : \
-/home/ahnh/3260-app/ResearchPal/platforms/android/AndroidManifest.xml \
--- ResearchPal/platforms/android/bin/HelloWorld-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/ahnh/3260-app/ResearchPal/platforms/android/bin/HelloWorld-debug-unaligned.apk : \
-/home/ahnh/3260-app/ResearchPal/platforms/android/bin/HelloWorld.ap_ \
-/home/ahnh/3260-app/ResearchPal/platforms/android/bin/classes.dex \
-/home/ahnh/3260-app/ResearchPal/platforms/android/libs/cordova-3.0.0.jar \
