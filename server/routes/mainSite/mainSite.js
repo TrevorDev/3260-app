f687e120ff198e70670a9944529024af3276cdd6
unknown <apang@uoguelph.ca>
2013-11-22 03:52:41 -0500

    Modified dashboard, createform, appliants, and group pages

--- server/routes/mainSite/mainSite.js
+++ server/routes/mainSite/mainSite.js
@@ -95,6 +95,9 @@ exports.showCreateForm = function(req, res, next) {
     
     /*Determine if the user is logged in*/
     if(researchAuth.auth(req)){
+        res.template=new Object();
+        res.template.username = req.session.username;
+        
         exports.render(req, res, next, 'researcherPortal/' + view);
     }else{
         res.redirect('/');
@@ -140,6 +143,7 @@ exports.showApplicants = function(req, res, next) {
         userM.getApplicant(req.params.id, function(applyData){
             res.template=new Object();
             res.template.applyData = applyData;
+            res.template.username = req.session.username;
             
             /*Direct user to the applicant's application page*/
             exports.render(req, res, next, 'researcherPortal/' + view);
