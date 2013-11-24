ffa516d3a50f0e8e0952b83b6d7c4ea403b05642
unknown <apang@uoguelph.ca>
2013-11-24 00:45:19 -0500

    Registration page for Researcher implemented

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
-//NODE JS modules
+/*NODE JS modules*/
 var express = require('express');
 var ejs = require('ejs');
 var app = express();
 
-//CUSTOM modules
+/*CUSTOM modules*/
 var rek = require('rekuire');
 var db = rek('database.js');
 db.connect();
@@ -15,53 +15,64 @@ var applyForm = rek('applyForm.js');
 var messaging = rek('messaging.js');
 var userModel = rek('userModel.js');
 
-//setup ejs with views folder
+/*setup ejs with views folder*/
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-
 
 app.use(express.cookieParser('secret111'));
 app.use(express.cookieSession());
 app.use(express.bodyParser());
 app.use(app.router);
 
-//static requests
+
+/*static requests*/
 app.get('/public/*', function(req, res, next){
     express.static(__dirname)(req, res, function(){next('route')});
 });
 
+//Logon Requests
 app.get('/logon', mainSite.showLogon);
 app.get('/user', researchAuth.logout);
 app.get('/researchAuth/logout', researchAuth.logout);
 app.get('/researchAuth/auth', researchAuth.auth);
 
+//Project Handling Requests
 app.get('/projects', mainSite.showProjects);
 app.get('/applyForm/:id', applyForm.getApplyForm);
 app.get('/apply/:id', mainSite.showApply);
 
+//Registeration Requests
+app.get('/register', mainSite.showRegistration);
+
+//Project Creation Requests
 app.get('/createForm', mainSite.showCreateForm);
 app.get('/dashboard', mainSite.showDashboard);
 app.get('/group/:id', mainSite.showGroupParticipant);
 
+//Participant Requests
 app.get('/message/:participantID', mainSite.showMessage);
 app.get('/message', mainSite.getMessages);
 app.get ('/updateMsg/:msgID', mainSite.updateDiaryStatus);
 
+//Applicant Requests
 app.get('/approveApp/:id', mainSite.approveApp);
 app.get('/rejectApp/:id', mainSite.rejectApp);
 app.get('/applicants/:id', mainSite.showApplicants);
 
+//Session Handler Requests
 app.get('/appAuth/check', appAuth.auth);
 app.get('/appAuth/logout', appAuth.logout);
 app.get('/uploads/:fileName', messaging.getRecording);
 app.get('/*', mainSite.showMainPage);
 
-//posts requests
+
+/*posts requests*/
 app.post('/appAuth/login', appAuth.validateUser);
 app.post('/researchAuth', researchAuth.login);
 app.post('/createGroup', applyForm.createGroup);
 app.post('/submitApplication', applyForm.submitForm);
 app.get('/approveApplicant/:participantID', userModel.approveApplicant);
+app.post('/createResearcherAcc', applyForm.addResearcher);
 app.post('/newRecording', messaging.addRecording);
 app.post('/sendTextMessage', messaging.sendTextMessage);
 app.listen(80);
--- server/routes/applyForm/applyForm.js
+++ server/routes/applyForm/applyForm.js
@@ -1,11 +1,46 @@
+/*********************************************************************
+* PROJECT: ResearchPal
+* FILE NAME: userModel.js
+*
+* AUTHOR: 
+*   Heesung Ahn
+*   Trevor Baron
+*   Anuj Bhatia
+*   Angela Pang
+*   Dan Robinson 
+*
+* DATE CREATED: 01/10/2013
+* DATE LAST MODIFIED: 25/11/2013
+*
+* DESCRIPTION:
+*   Contains the functions that handles user information to and 
+*   from the database on the server.
+*********************************************************************/
+/*NodeJS Modules*/
 var fs = require('fs');
 var ejs = require('ejs');
 var rek = require('rekuire');
 
+/*Custom Modules*/
 var formM = rek('formModel.js');
 var groupM = rek('groupModel.js');
 var userM = rek('userModel.js');
 
+
+/*Submit Researcher Registration*/
+exports.addResearcher = function(req, res, next) {
+    userM.checkUsername(req.body.username,function(exists){
+        if (exists == false) {
+            userM.createResearcher(req.body.fname,req.body.lname,req.body.email,req.body.username,req.body.password,function(success){
+                
+            });
+        }
+        res.send(exists);
+    });
+};
+
+
+/*Returns Project Information*/
 exports.getApplyForm = function(req, res, next) {
 	var groupID=null;
 	if(req.params.id){
@@ -17,6 +52,8 @@ exports.getApplyForm = function(req, res, next) {
 	});
 };
 
+
+/*Create Project Group*/
 exports.createGroup = function(req, res, next) {
 	groupM.createGroup(req.body.name,req.body.keyword,req.body.start,req.body.end,req.session.userID,function(success,groupID){
 		formM.createForm(req.body.questions,groupID,function(success){
@@ -25,6 +62,8 @@ exports.createGroup = function(req, res, next) {
 	});
 };
 
+
+/*Store Applicant Information*/
 exports.submitForm = function(req, res, next) {
 	userM.createParticipant(req.body.fname,req.body.lname,req.body.email,req.body.groupID,function(userid){
 		formM.submitApplication(req.body.fname,req.body.lname,req.body.answers,req.body.groupID,userid,function(){
@@ -32,3 +71,4 @@ exports.submitForm = function(req, res, next) {
 		});
 	});
 };
+
