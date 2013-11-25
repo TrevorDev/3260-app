3bb379f949b7ca8619b4efaa30215878151f5a40
root <root@TBstylez.(none)>
2013-11-25 10:59:46 -0500

    email name

--- server/custom_modules/email/email.js
+++ server/custom_modules/email/email.js
@@ -24,7 +24,7 @@ var smtpTransport = nodemailer.createTransport("SMTP",{
 
 exports.sendEmail = function(to,subject,text){
         var mailOptions = {
-            from: "DevBrick <researchPalHelper@gmail.com>", // sender address
+            from: "Research Pal <researchPalHelper@gmail.com>", // sender address
             to: to, // list of receivers
             subject: subject, // Subject line
             text: text, // plaintext body
